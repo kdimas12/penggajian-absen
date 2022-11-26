25aa12cb224e8fdc88b3edb0d758af2285321936
Dimas <dimaskurniawan40@gmail.com>
2022-11-27 04:43:49 +0700

    :lipstick: Add adminlte, layout and partials

---
+++ app/Views/layout/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<?= $this->include('layout/head') ?>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?= $this->include('layout/navbar') ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?= $this->include('layout/sidebar') ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <?= $this->include('layout/header') ?>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <div class="content">
+                <?= $this->renderSection('content') ?>
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+                <h5>Title</h5>
+                <p>Sidebar content</p>
+            </div>
+        </aside>
+        <!-- /.control-sidebar -->
+        <!-- Main Footer -->
+        <?= $this->include('layout/footer') ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url('plugins/jquery/jquery.min.js') ?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url('assets/js/adminlte.min.js') ?>"></script>
+</body>
+
+</html>
