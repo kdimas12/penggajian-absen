2fbb4ef93cd9c370e2d1287a55fb47174ec14a80
Dimas <dimaskurniawan40@gmail.com>
2022-12-11 06:04:31 +0700

    :wrench: Add method Ubah and add column Aksi

--- app/Controllers/AdminJabatan.php
+++ app/Controllers/AdminJabatan.php
@@ -31,8 +31,32 @@ class AdminJabatan extends BaseController
         echo view('jabatan_tambah');
     }
 
+    public function ubah($id_jabatan)
+    {
+        $jabatan = new JabatanModel();
+        $data['jabatan'] = $jabatan->where('id_jabatan', $id_jabatan)->first();
+
+        $validation = \Config\Services::validation();
+        $validation->setRules(['nama_jabatan' => 'required']);
+        $isDataValid = $validation->withRequest($this->request)->run();
+
+        if ($isDataValid) {
+            $jabatan->update($id_jabatan, [
+                'nama' => $this->request->getPost('nama_jabatan'),
+                'uang_lembur' => $this->request->getPost('uang_lembur'),
+                'uang_makan' => $this->request->getPost('uang_makan')
+            ]);
+            return redirect()->to('admin/jabatan');
+        }
+        echo view('jabatan_ubah', $data);
+    }
+
     public function loadData()
     {
-        return datatables('tbl_jabatan')->make();
+        return datatables('tbl_jabatan')->addColumn('aksi', function ($data) {
+            $urlUbah = base_url('admin/jabatan/' . $data->id_jabatan . '/ubah');
+            $urlHapus = base_url('admin/jabatan/' . $data->id_jabatan . '/hapus');
+            return '<div class="btn-group"><a href="' . $urlUbah . '" class="btn btn-primary"><i class="fas fa-edit"></i></a><a href="' . $urlHapus . '" class="btn btn-danger"><i class="fas fa-trash"></i></a></div>';
+        })->rawColumns(['aksi'])->make();
     }
 }
