b480cf45467172450c0d3848640cafe4abd9a66c
Dimas <dimaskurniawan40@gmail.com>
2022-12-10 23:22:18 +0700

    :memo: Add language datatables

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -5,6 +5,21 @@ $(function () {
       lengthChange: false,
       autoWidth: false,
       buttons: ["copy", "csv", "excel", "pdf", "print"],
+      language: {
+        sProcessing: "Sedang memproses ...",
+        lengthMenu: "Tampilkan _MENU_ data per halaman",
+        zeroRecord: "Maaf data tidak tersedia",
+        info: "Menampilkan _PAGE_ halaman dari _PAGES_ halaman",
+        infoEmpty: "Tidak ada data yang tersedia",
+        infoFiltered: "(difilter dari _MAX_ total data)",
+        sSearch: "Cari",
+        oPaginate: {
+          sFirst: "Pertama",
+          sPrevious: "Sebelumnya",
+          sNext: "Selanjutnya",
+          sLast: "Terakhir",
+        },
+      },
     })
     .buttons()
     .container()
