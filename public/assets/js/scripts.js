c408ebd7bd76a990161e904212baa00f72963e38
Dimas <dimaskurniawan40@gmail.com>
2022-12-11 02:57:25 +0700

    :pencil2: Modify Datatables configuration

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,29 +1,35 @@
 $(function () {
-  $("#jabatan-table")
-    .DataTable({
-      responsive: true,
-      lengthChange: false,
-      autoWidth: false,
-      buttons: ["copy", "csv", "excel", "pdf", "print"],
-      language: {
-        sProcessing: "Sedang memproses ...",
-        lengthMenu: "Tampilkan _MENU_ data per halaman",
-        zeroRecord: "Maaf data tidak tersedia",
-        info: "Menampilkan _PAGE_ halaman dari _PAGES_ halaman",
-        infoEmpty: "Tidak ada data yang tersedia",
-        infoFiltered: "(difilter dari _MAX_ total data)",
-        sSearch: "Cari",
-        oPaginate: {
-          sFirst: "Pertama",
-          sPrevious: "Sebelumnya",
-          sNext: "Selanjutnya",
-          sLast: "Terakhir",
-        },
+  $("#jabatan-table").DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: "http://localhost:8080/admin/jabatan/json",
+    },
+    columns: [
+      { data: "id_jabatan", name: "id_jabatan" },
+      { data: "nama", name: "nama" },
+      { data: "uang_lembur", name: "uang_lembur" },
+      { data: "uang_makan", name: "uang_makan" },
+    ],
+    responsive: true,
+    lengthChange: false,
+    autoWidth: false,
+    language: {
+      sProcessing: "Sedang memproses ...",
+      lengthMenu: "Tampilkan _MENU_ data per halaman",
+      zeroRecord: "Maaf data tidak tersedia",
+      info: "Menampilkan _PAGE_ halaman dari _PAGES_ halaman",
+      infoEmpty: "Tidak ada data yang tersedia",
+      infoFiltered: "(difilter dari _MAX_ total data)",
+      sSearch: "Cari",
+      oPaginate: {
+        sFirst: "Pertama",
+        sPrevious: "Sebelumnya",
+        sNext: "Selanjutnya",
+        sLast: "Terakhir",
       },
-    })
-    .buttons()
-    .container()
-    .appendTo("#jabatan-table_wrapper .col-md-6:eq(0)");
+    },
+  });
 
   $("#form-jabatan").validate({
     rules: {
