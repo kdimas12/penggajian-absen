294a044cc323f4c4902a933871e54a17a9478dfe
Dimas <dimaskurniawan40@gmail.com>
2022-12-15 22:17:24 +0700

    :wrench: Add function Tambah

--- app/Controllers/AdminWilayah.php
+++ app/Controllers/AdminWilayah.php
@@ -3,11 +3,38 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\WilayahModel;
 
 class AdminWilayah extends BaseController
 {
     public function index()
     {
         return view('wilayah');
+    }
+
+    public function tambah()
+    {
+        $validation = \Config\Services::validation();
+        $validation->setRules(['nama_wilayah' => 'required']);
+        $isDataValid = $validation->withRequest($this->request)->run();
+
+        if ($isDataValid) {
+            $wilayah = new WilayahModel();
+            $wilayah->insert([
+                'nama' => $this->request->getPost('nama_wilayah'),
+            ]);
+            return redirect()->to('admin/wilayah');
+        }
+
+        echo view('wilayah_tambah');
+    }
+
+    public function loadData()
+    {
+        return datatables('tbl_wilayah')->addColumn('aksi', function ($data) {
+            $urlUbah = base_url('admin/wilayah/' . $data->id_wilayah . '/ubah');
+            $urlHapus = base_url('admin/wilayah/' . $data->id_wilayah . '/hapus');
+            return '<div class="btn-group"><a href="' . $urlUbah . '" class="btn btn-primary"><i class="fas fa-edit"></i></a><a href="' . $urlHapus . '" class="btn btn-danger"><i class="fas fa-trash"></i></a></div>';
+        })->rawColumns(['aksi'])->make();
     }
 }
