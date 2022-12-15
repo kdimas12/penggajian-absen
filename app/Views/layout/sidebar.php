32d8c2e44ff6cdb2fe2736220a4e33d53057714e
Dimas <dimaskurniawan40@gmail.com>
2022-12-16 06:55:20 +0700

    :lipstick: Add logout button and change some link path

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?= base_url('admin') ?>" class="brand-link">
         <img src="<?= base_url('assets/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -22,7 +22,7 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="<?= base_url('admin') ?>" class="nav-link">
                         <i class="nav-icon fas fa-home"></i>
                         <p>Beranda</p>
                     </a>
@@ -73,6 +73,9 @@
                         <i class="nav-icon fas fa-file-alt"></i>
                         <p>Laporan</p>
                     </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="d-block btn btn-danger">Logout</a>
                 </li>
             </ul>
         </nav>
