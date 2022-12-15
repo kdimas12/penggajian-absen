213ec5902cb29152e8ab49bd5716f7536d28b1b9
Dimas <dimaskurniawan40@gmail.com>
2022-12-16 01:12:36 +0700

    :wrench: Add tambah function

--- app/Controllers/AdminPegawai.php
+++ app/Controllers/AdminPegawai.php
@@ -3,11 +3,41 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\PegawaiModel;
 
 class AdminPegawai extends BaseController
 {
     public function index()
     {
         return view('pegawai');
+    }
+
+    public function tambah()
+    {
+        $validation = \Config\Services::validation();
+        $validation->setRules([
+            'nama_pegawai' => 'required',
+            'nip' => 'required'
+        ]);
+        $isDataValid = $validation->withRequest($this->request)->run();
+
+        if ($isDataValid) {
+            $wilayah = new PegawaiModel();
+            $wilayah->insert([
+                'nama' => $this->request->getPost('nama_pegawai'),
+                'nip' => $this->request->getPost('nip'),
+                'password' => "",
+                'id_jabatan' => $this->request->getPost('jabatan'),
+                'id_wilayah' => $this->request->getPost('wilayah')
+            ]);
+            return redirect()->to('admin/pegawai');
+        }
+
+        echo view('pegawai_tambah');
+    }
+
+    public function loadData()
+    {
+        return datatables('tbl_pegawai')->make();
     }
 }
