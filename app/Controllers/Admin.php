b8b83410c80a03240a763cb5831827f619f8a169
Dimas <dimaskurniawan40@gmail.com>
2022-12-16 06:51:04 +0700

    :wrench: Add model to send to view

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,11 +3,22 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\JabatanModel;
+use App\Models\PegawaiModel;
+use App\Models\WilayahModel;
 
 class Admin extends BaseController
 {
     public function index()
     {
-        return view('admin');
+        $pegawai = new PegawaiModel();
+        $wilayah = new WilayahModel();
+        $jabatan = new JabatanModel();
+        $data = [
+            'pegawai' => $pegawai->countAllResults(),
+            'wilayah' => $wilayah->countAllResults(),
+            'jabatan' => $jabatan->countAllResults()
+        ];
+        return view('admin', $data);
     }
 }
