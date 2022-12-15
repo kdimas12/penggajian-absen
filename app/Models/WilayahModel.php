34e351af312eb11ffab9f5140363b3cb067852b5
Dimas <dimaskurniawan40@gmail.com>
2022-12-15 22:17:42 +0700

    :wrench: Add Model

---
+++ app/Models/WilayahModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class WilayahModel extends Model
+{
+    protected $table = 'tbl_wilayah';
+    protected $primaryKey = 'id_wilayah';
+
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['nama'];
+}
